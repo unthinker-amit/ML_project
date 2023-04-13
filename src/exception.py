14778b039a7660edc6dd7faec5bc381840c1beea
Amit Kumar <Amit.Kumar@unthinkable.co>
2023-04-13 10:02:37 +0530

    formatted exception file

--- src/exception.py
+++ src/exception.py
@@ -1,11 +1,12 @@
 import sys
 from src.logger import logging
 
+
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
 
-    error_message = f"Error occured in python script name [{file_name}] line number [{exc_tb.tb_lineno}] error message [{str(error)}]"
+    error_message = f"Error occurred in python script name [{file_name}] line number [{exc_tb.tb_lineno}] error message [{str(error)}]"
     return error_message
 
 
@@ -19,10 +20,11 @@ class CustomException(Exception):
         return self.error_message
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
 
     try:
-        a=1/0
+        logging.info("Logging file started")
+        a = 1/0
     except Exception as err:
-        logging.error("can not divide with zero")
-        raise CustomException(err,sys)
+        logging.error("can not divide with zero \n",exc_info=True)
+        raise CustomException(err, sys)
